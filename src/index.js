a60691bd71c7d8b3da686d16e2ef67041b16e5b1
Thomas Sankara <thomassankara74@gmail.com>
2022-03-15 17:43:38 +0300

    add context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import { CartContextProvider } from "./context/context"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
